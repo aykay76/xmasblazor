90d9b5cfd3bfb0fff065f62f28ec74a37d58f15e
Alan Kelly <alankelly@Alans-MacBook.home>
2020-12-02 18:39:33 +0000

    adding SPI and page to trigger

--- Data/ChristmasTreeService.cs
+++ Data/ChristmasTreeService.cs
@@ -1,17 +1,29 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.Device.Spi;
+using System.Collections.Generic;
 
 namespace xmasblazor.Data
 {
-    public class WeatherForecastService
+    public class ChristmasTreeService
     {
-        private SpiDevice tree;
-
-        public Task<WeatherForecast[]> TurnOnAsync(DateTime startDate)
+        public void TurnOn()
         {
-            tree = new SpiDevice();
+            SpiConnectionSettings settings = new SpiConnectionSettings(0);
+            settings.Mode = SpiMode.Mode3;
+
+            SpiDevice device = SpiDevice.Create(settings);
+
+            List<byte> data = new List<byte>();
+            data.AddRange(new byte[] { 0, 0, 0, 0 }); // start frame
+            for (int i = 0; i < 25; i++)
+            {
+                data.AddRange(new byte[] { 1, 1, 1, 1 });
+            }
+            data.AddRange(new byte[] { 0, 0, 0, 0, 0 });
+
+            device.Write(data.ToArray());
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+My attempt to port the PiHut 3D RGB LED xmas tree code from:
+
+ https://github.com/ThePiHut/rgbxmastree#rgbxmastree
